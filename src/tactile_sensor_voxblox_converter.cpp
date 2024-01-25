52cdd790c19f1de7516a5762c638cb748bc60326
Takuma-Hiraoka <t-hiraoka@jsk.imi.i.u-tokyo.ac.jp>
2024-01-25 13:20:35 +0900

    publish tsdf_layer

--- src/tactile_sensor_voxblox_converter.cpp
+++ src/tactile_sensor_voxblox_converter.cpp
@@ -1,10 +1,20 @@
 #include <ros/ros.h>
 #include <algorithm>
+#include <memory>
 #include <sensor_msgs/PointCloud2.h>
 #include <std_msgs/Float32MultiArray.h>
+#include <voxblox/utils/color_maps.h>
+#include <voxblox/core/tsdf_map.h>
+#include <voxblox/integrator/tsdf_integrator.h>
+#include <voxblox_ros/conversions.h>
+#include <voxblox_ros/ros_params.h>
+#include <voxblox_msgs/Layer.h>
+#include <minkindr_conversions/kindr_tf.h>
 #include <pcl_conversions/pcl_conversions.h>
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
+#include <tf2_ros/buffer.h>
+#include <tf2_ros/transform_listener.h>
 #include <cnoid/EigenTypes>
 #include <cnoid/YAMLReader>
 
@@ -13,17 +23,22 @@ namespace tactile_sensor_voxblox_converter {
   {
   public:
     tactile_sensor_voxblox_converter()
+      : tfListener_(tfBuffer_)
     {
       ros::NodeHandle pnh("~");
 
-      pnh.param("output", output_tsdf_layer_,
-                std::string(""));
+      pnh.param("world_frame", world_frame_,
+                std::string("odom_ground"));
       pnh.param("config_filename", config_filename_,
                 std::string(""));
       pnh.param("debug", debug_,
                 0);
 
+      tsdf_map_pub_ = pnh.advertise<voxblox_msgs::Layer>("output", 1, false);
       tactile_sub_ = pnh.subscribe("input", 1, &tactile_sensor_voxblox_converter::topicCallback, this);
+      color_map_.reset(new voxblox::GrayscaleColorMap());
+      tsdf_map_.reset(new voxblox::TsdfMap(voxblox::getTsdfMapConfigFromRosParam(pnh)));
+      tsdf_integrator_.reset(new voxblox::FastTsdfIntegrator(voxblox::getTsdfIntegratorConfigFromRosParam(pnh), tsdf_map_->getTsdfLayerPtr()));
 
       // load tactile_sensor_file
       {
@@ -86,33 +101,62 @@ namespace tactile_sensor_voxblox_converter {
       }
 
       // cloudsを整形してPointCloud2へ
-      std::vector<sensor_msgs::PointCloud2> cloud_msgs;
       for (int i=0; i < clouds.size(); i++) {
 	clouds[i].width = clouds[i].points.size();
 	clouds[i].height = 1;
-	sensor_msgs::PointCloud2 cloud_msg;
-	pcl::toROSMsg(clouds[i], cloud_msg);
-	cloud_msg.header.frame_id = sensorFrames_[i];
-	cloud_msgs.push_back(cloud_msg);
-      }
-
-      // デバッグ用publish
-      if (debug_ >= 1) {
-	for (int i=0; i< pointcloud_pubs_.size(); i++) {
-	  pointcloud_pubs_[i].publish(cloud_msgs[i]);
+	// デバッグ用publish
+	if (debug_ >= 1) {
+	  sensor_msgs::PointCloud2 cloud_msg;
+	  pcl::toROSMsg(clouds[i], cloud_msg);
+	  cloud_msg.header.frame_id = sensorFrames_[i];
+	  pointcloud_pubs_[i].publish(cloud_msg);
 	}
       }
+
+      // convertion. see https://github.com/ethz-asl/voxblox/blob/master/voxblox_ros/src/tsdf_server.cc#L213
+      for (int i=0; i < clouds.size(); i++) {
+	voxblox::Pointcloud points_C;
+	voxblox::Colors colors;
+	voxblox::convertPointcloud(clouds[i], color_map_, &points_C, &colors);
+	geometry_msgs::TransformStamped trans;
+	try{
+        trans = tfBuffer_.lookupTransform(world_frame_, sensorFrames_[i], ros::Time(0), ros::Duration(3));
+	} catch (std::exception& ex) {
+	  ROS_ERROR_STREAM(ex.what());
+	  return;
+	}
+	voxblox::Transformation T_G_C;
+	tf::transformTFToKindr(tf::Transform(tf::Quaternion(trans.transform.rotation.x,
+							    trans.transform.rotation.y,
+							    trans.transform.rotation.z,
+							    trans.transform.rotation.w),
+					     tf::Vector3(trans.transform.translation.x,
+							 trans.transform.translation.y,
+							 trans.transform.translation.z)),
+			       &T_G_C);
+	tsdf_integrator_->integratePointCloud(T_G_C, points_C, colors, false); // is_freespace_pointcloud
+      }
+
+      // publish
+      voxblox_msgs::Layer layer_msg;
+      voxblox::serializeLayerAsMsg<voxblox::TsdfVoxel>(this->tsdf_map_->getTsdfLayer(),
+						       true, &layer_msg, voxblox::MapDerializationAction::kMerge);
+      this->tsdf_map_pub_.publish(layer_msg);
     }
 
   protected:
     ros::NodeHandle nh_;
 
+    tf2_ros::Buffer tfBuffer_;
+    tf2_ros::TransformListener tfListener_;
     std::vector<ros::Publisher> pointcloud_pubs_;
+    ros::Publisher tsdf_map_pub_;
     ros::Subscriber tactile_sub_;
-    std::string output_tsdf_layer_;
+    std::string world_frame_;
     std::string config_filename_;
     double contact_threshould_ = 5;
     int debug_ = 0;
+
     class TactileSensor
     {
     public:
@@ -121,6 +165,9 @@ namespace tactile_sensor_voxblox_converter {
     };
     std::vector<TactileSensor> tactileSensorList_;
     std::vector<std::string> sensorFrames_;
+    std::shared_ptr<voxblox::ColorMap> color_map_;
+    std::shared_ptr<voxblox::TsdfMap> tsdf_map_;
+    std::unique_ptr<voxblox::TsdfIntegratorBase> tsdf_integrator_;
   };
 }
 
